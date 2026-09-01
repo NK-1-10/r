c8f1908edc058bb21429f2d5d2a623674c36e2cd
NK-1-10 <kanepe.nikola@gmail.com>
2026-09-01 16:15:09 +0300

    start box breaking

--- intro/script/start.gd
+++ intro/script/start.gd
@@ -3,11 +3,18 @@ extends Node2D
 @onready var box = $box
 @onready var info = $explain/Panel
 @onready var mark = $respawn
+@onready var ground = $ground
+
+@export var stiffness = 600.0
+@export var damping = 25.0
+
 
 var panelStart = Vector2(87.0, 1473.0)
 var panelEnd = Vector2(87.0, 218.0)
 var boxStart = Vector2(1229, -742)
 var mouseLocation = Vector2()
+var offset = Vector2.ZERO
+
 
 var intro = {
 	1 :{
@@ -19,9 +26,13 @@ var n = 1
 var waitingForClick = false
 var click = 0
 var holdBox = false;
+var first = true;
+var drops = 0
+var waitForDrop = false
 
  # ready and process --------------------------------------------------------------------------------------
 func _ready() -> void:
+	box.body_entered.connect(_on_box_body_entered)
 	box.freeze = true
 	await get_tree().create_timer(1).timeout
 	Signals.text_toggle.emit("up")
@@ -32,7 +43,16 @@ func _ready() -> void:
 
 func _process(delta: float) -> void:
 	if holdBox:
-		box.position = get_global_mouse_position()
+		if first:
+			phase2()
+			first = false
+		var grab_point = box.to_global(offset)
+		var to_target = get_global_mouse_position() - grab_point
+		var force = to_target * stiffness - box.linear_velocity * damping
+		force = force.limit_length(2000.0)  # tune this cap
+		box.apply_force(force, grab_point - box.global_position)
+	if waitForDrop:
+		pass
 	
 # intro --------------------------------------------------------------------------------------------------------
 
@@ -58,6 +78,7 @@ func _input(event: InputEvent) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
 		if event.pressed and waitingForClick:
 			holdBox = true
+			offset = box.to_local(get_global_mouse_position())
 		else:
 			holdBox = false
 			box.freeze = false
@@ -71,3 +92,14 @@ func _on_out_body_exited(body: Node2D) -> void:
 		body.angular_velocity = 0.0
 	else:
 		body.position = mark.position
+
+# after pick up box ------------------------------------------------------------------------------------------------------
+
+func phase2():
+	await Global.Typewriter("Hmm...", $explain/Panel/header, false)
+	await Global.Typewriter("Seems rather sterdy...", $explain/Panel/tut, true)
+	waitForDrop = true
+
+func _on_box_body_entered(body: Node) -> void:
+	if body == ground:
+		print("box touched ground!")
